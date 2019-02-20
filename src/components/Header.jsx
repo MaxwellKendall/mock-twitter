c95e942c5082abde30d5354c1cd99e2707b2ef69
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-02-20 15:55:22 -0500

    removing FA icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 
 export const Header = ({ setModal }) => {
   return (
     <div className="header">
       <h1>Mock Twitter</h1>
-      <FontAwesomeIcon onClick={() => setModal(true)} icon={faPencilAlt} />
+      <button onClick={() => setModal(true)} className="header__tweet-btn">
+        Tweet
+      </button>
     </div>
   );
 };
